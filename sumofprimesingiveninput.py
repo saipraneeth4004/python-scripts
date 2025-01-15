3ee3fb0ba4e7baa70a1fd82763ed8c7d7ee89cfc
Saipraneeth <2300031549@kluniversity.in>
2025-01-15 22:01:10 +0530

    Create sumofprimesingiveninput.py

---
+++ sumofprimesingiveninput.py
@@ -0,0 +1,13 @@
+n=int(input())
+sum=0
+for i in range(n):
+    k=int(input())
+    if(k!=1):
+        fact=0
+        for j in range(2,k):
+            if(k%j==0):
+                fact=1 
+        if(fact==0):
+            sum=sum+k
+print(sum)
+
